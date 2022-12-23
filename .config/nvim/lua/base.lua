a275c93b53a5f11d25fd125ce9cc7eed87015eda
masahirookubo <stmhamachiii@gmail.com>
2022-12-24 01:01:08 +0900

    added plugins

--- .config/nvim/lua/base.lua
+++ .config/nvim/lua/base.lua
@@ -4,6 +4,9 @@ vim.scriptencoding = 'utf-8'
 vim.opt.encoding = 'utf-8'
 vim.opt.fileencoding = 'utf-8'
 
+
+vim.api.nvim_set_keymap('i', 'jj', '<ESC>', {silent=true})
+
 vim.wo.number = true
 
 vim.opt.title = true
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,40 @@
+local status, packer = pcall(require, 'packer')
+if (not status) then
+  print("Packer is not installed")
+  return
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+packer.startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use {
+    'svrana/neosolarized.nvim',
+    requires = { 'tjdevries/colorbuddy.nvim' }
+  }
+  use 'hoob3rt/lualine.nvim' -- Statusline
+  use 'nvim-lua/plenary.nvim' -- Common utilities
+  use 'onsails/lspkind-nvim' -- vscode-like pictograms
+  use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
+  use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in LSP
+  use 'hrsh7th/nvim-cmp' -- Completion
+  use 'neovim/nvim-lspconfig' -- LSP
+  use 'glepnir/lspsaga.nvim' -- LSP UIs
+  use 'MunifTanjim/prettier.nvim'
+  use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
+  use 'kyazdani42/nvim-web-devicons' -- File icons
+  use 'nvim-telescope/telescope.nvim'
+  use 'nvim-telescope/telescope-file-browser.nvim'
+  use 'williamboman/nvim-lsp-installer'
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+  use 'windwp/nvim-autopairs'
+  use 'windwp/nvim-ts-autotag'
+  use 'akinsho/nvim-bufferline.lua'
+  use 'norcalli/nvim-colorizer.lua'
+
+  use 'lewis6991/gitsigns.nvim'
+  use 'dinhhuy258/git.nvim' -- For git blame & browse
+end)
