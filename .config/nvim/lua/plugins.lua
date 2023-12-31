bae929b725c43dff4bf1d0ebfab6fe9bda046c4e
masahirookubo <stmhamachiii@gmail.com>
2023-12-31 22:10:02 +0900

    feat: コメントアウト1

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -20,48 +20,45 @@ packer.startup(function(use)
   use 'neovim/nvim-lspconfig' -- LSP
   -- 最新版だとエラーが出るのでcommit指定してる
   use { 'glepnir/lspsaga.nvim', commit = '335805d4f591f5bb71cabb6aa4dc58ccef8e8617' } -- LSP UIs
-  -- use { 'glepnir/lspsaga.nvim' } -- LSP UIs
   use 'williamboman/mason.nvim'
   use 'williamboman/mason-lspconfig.nvim'
-  use { 'j-hui/fidget.nvim', tag = 'legacy' } -- Lsp loading indicator
+  -- use { 'j-hui/fidget.nvim', tag = 'legacy' } -- Lsp loading indicator
   use 'MunifTanjim/prettier.nvim'
-  use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
+  -- use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
   use 'kyazdani42/nvim-web-devicons' -- File icons
   use 'nvim-telescope/telescope.nvim'
   use 'nvim-telescope/telescope-file-browser.nvim'
   -- use { 'nvim-treesitter/nvim-treesitter' } -- Code color scheme
-  use {
-    'nvim-treesitter/nvim-treesitter',
-  }
+  use { 'nvim-treesitter/nvim-treesitter' }
   use 'windwp/nvim-autopairs'
   use 'kylechui/nvim-surround' -- Surround text with selected charactors
   use 'akinsho/bufferline.nvim'
-  use 'norcalli/nvim-colorizer.lua'
+  use 'norcalli/nvim-colorizer.lua' -- Colorize hex color
   -- use 'L3MON4D3/LuaSnip'
-  use 'rafamadriz/friendly-snippets'
+  -- use 'rafamadriz/friendly-snippets'
   use { 'numToStr/Comment.nvim', requires = { 'JoosepAlviste/nvim-ts-context-commentstring' } }
   -- use { 'numToStr/Comment.nvim' }
   use 'akinsho/toggleterm.nvim'
   use 'rlane/pounce.nvim' -- Easy motion
-  use 'lewis6991/gitsigns.nvim' -- Show git change on buffers on left
-  use 'akinsho/git-conflict.nvim' -- Git client to fix conflict
-  use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' } -- Git diff view
+  -- use 'lewis6991/gitsigns.nvim' -- Show git change on buffers on left
+  -- use 'akinsho/git-conflict.nvim' -- Git client to fix conflict
+  -- use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' } -- Git diff view
   -- Image viewer
-  use { 'samodostal/image.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+  -- use { 'samodostal/image.nvim', requires = { 'nvim-lua/plenary.nvim' } }
   use { 'github/copilot.vim' }
 
   -- Vim scripts under this line
-  use { 'ntpeters/vim-better-whitespace' }
-  use { 'ElmCast/elm-vim', ft = { 'elm' } }
-  use { 'simeji/winresizer' } -- Change buffer size
+  -- use { 'ntpeters/vim-better-whitespace' }
+  -- use { 'ElmCast/elm-vim', ft = { 'elm' } }
+  -- use { 'simeji/winresizer' } -- Change buffer size
   use { 'tpope/vim-fugitive' } -- git
   -- use { 'jiangmiao/auto-pairs' } -- Auto pairs
-  use { 'nathanaelkane/vim-indent-guides' } -- Show indent
-  use { 'iberianpig/tig-explorer.vim' }
-  use { 'slim-template/vim-slim' } -- TODO: Add ft
+  -- use { 'nathanaelkane/vim-indent-guides' } -- Show indent
+  -- use { 'iberianpig/tig-explorer.vim' }
+  -- use { 'slim-template/vim-slim' } -- TODO: Add ft
   use { 'tpope/vim-rails' } -- TODO: Add ft
   use { 'rust-lang/rust.vim' }
-  use { 'zah/nim.vim' } -- Nim syntax highlighter
-  use { 'hashivim/vim-terraform' } -- Terraform formatter
-  use { 'chrisbra/csv.vim' } -- CSV visualizer
+  -- use { 'zah/nim.vim' } -- Nim syntax highlighter
+  -- use { 'hashivim/vim-terraform' } -- Terraform formatter
+  -- use { 'chrisbra/csv.vim' } -- CSV visualizer
 end)
